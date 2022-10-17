56f467e365adf9c379f440bb18831353a3b17802
julien belinga <julienbelinga29@gmail.com>
2022-10-18 00:08:51 +0200

    starting implementing effects

---
+++ script.js
@@ -0,0 +1,10 @@
+const slidingProject = document.querySelector('#test');
+
+console.log(slidingProject);
+
+// window.addEventListener('scroll', () =>{
+
+//     const{scrollTop, clientHeight} = document.documentElement;
+
+//     console.log(slidingProject.getBoundingClientRect());
+// })
