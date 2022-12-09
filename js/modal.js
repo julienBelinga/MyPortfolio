6831f9eddaf78f0f220016acdeb16262bf3aeaa3
julien belinga <julienbelinga29@gmail.com>
2022-12-09 16:23:22 +0100

    add sport-evolution project

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,21 @@
 // Get DOM Elements TODO: modify
 const modal = document.querySelector('#modal-einden');
 const modal2 = document.querySelector('#modal-portfolio');
+const modal3 = document.querySelector('#modal-sport-evolution');
 const modalBtn = document.querySelector('#modal-btn');
 const modalBtn2 = document.querySelector('#modal-btn2');
+const modalBtn3 = document.querySelector('#modal-btn3');
 const closeBtn = document.querySelector('.close');
 const closeBtn2 = document.querySelector('.close2');
+const closeBtn3 = document.querySelector('.close3');
 
 // Events
 modalBtn.addEventListener('click', openModal);
 modalBtn2.addEventListener('click', openModal2);
+modalBtn3.addEventListener('click', openModal3);
 closeBtn.addEventListener('click', closeModal);
 closeBtn2.addEventListener('click', closeModal);
+closeBtn3.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
 
 // Open
@@ -22,10 +27,15 @@ function openModal2() {
   modal2.style.display = 'block';
 }
 
+function openModal3() {
+  modal3.style.display = 'block';
+}
+
 // Close
 function closeModal() {
   modal.style.display = 'none';
   modal2.style.display = 'none';
+  modal3.style.display = 'none';
 }
 
 // Close If Outside Click
