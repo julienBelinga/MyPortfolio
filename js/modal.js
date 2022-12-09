19b89c3f297ca442380df7b95c1f7335902eb58b
julien belinga <julienbelinga29@gmail.com>
2022-12-09 15:36:29 +0100

    modal done v1

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,16 @@
 // Get DOM Elements TODO: modify
-const modal = document.querySelector('#my-modal');
+const modal = document.querySelector('#modal-einden');
+const modal2 = document.querySelector('#modal-portfolio');
 const modalBtn = document.querySelector('#modal-btn');
+const modalBtn2 = document.querySelector('#modal-btn2');
 const closeBtn = document.querySelector('.close');
+const closeBtn2 = document.querySelector('.close2');
 
 // Events
 modalBtn.addEventListener('click', openModal);
+modalBtn2.addEventListener('click', openModal2);
 closeBtn.addEventListener('click', closeModal);
+closeBtn2.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
 
 // Open
@@ -13,9 +18,14 @@ function openModal() {
   modal.style.display = 'block';
 }
 
+function openModal2() {
+  modal2.style.display = 'block';
+}
+
 // Close
 function closeModal() {
   modal.style.display = 'none';
+  modal2.style.display = 'none';
 }
 
 // Close If Outside Click
