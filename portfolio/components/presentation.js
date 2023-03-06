f3b9df12f13d3a4132bc287ee780e8b446e99c49
julien belinga <julienbelinga29@gmail.com>
2023-03-06 12:55:28 +0100

    save change

--- portfolio/components/presentation.js
+++ portfolio/components/presentation.js
@@ -4,34 +4,34 @@ import pp_entier from 'assets/img/pp_entier.png'
 function Presentation(){
     return(
         <>
-         <div id="mid-header">
-            <div id="profil" className="mobil">
-                <div id="profile-mobile-bg"></div>
-                <Image id="pp" src={pp_entier} alt="photo de profil"/>
-            </div>
-            <div className="desktop">
-                <div id="bg-title">Developpeur web</div>
-            </div>
-            <div id="desktop-header">
-                <div id="div">
-                    <h1>Julien BELINGA</h1> 
-                    <div style={{display: "flex", flexDirection: "column"}}>
-                        <div className="wrapper">
-                            <p className="static-txt">Je suis un</p>
-                            <ul className="dynamic-txts">
-                                <li><span>Développeur Front end</span></li>
-                                <li><span>Développeur Back end</span></li>
-                                <li><span>DEVOPS</span></li>
-                                <li><span>Freelance.</span></li>
-                            </ul>
-                        </div>
-                        <a id="contact-button" href="contact.html">Contactez moi</a>
-                    </div>
-                    
+            <div id="mid-header">
+                <div id="profil" className="mobil">
+                    <div id="profile-mobile-bg"></div>
+                    <Image id="pp" src={pp_entier} alt="photo de profil"/>
                 </div>
-                <Image className="desktop" src={pp_entier} alt="photo de profil"/>
+                <div className="desktop">
+                    <div id="bg-title">Developpeur web</div>
+                </div>
+                <div id="desktop-header">
+                    <div id="div">
+                        <h1>Julien BELINGA</h1> 
+                        <div style={{display: "flex", flexDirection: "column"}}>
+                            <div className="wrapper">
+                                <p className="static-txt">Je suis un</p>
+                                <ul className="dynamic-txts">
+                                    <li><span>Développeur Front end</span></li>
+                                    <li><span>Développeur Back end</span></li>
+                                    <li><span>DEVOPS</span></li>
+                                    <li><span>Freelance.</span></li>
+                                </ul>
+                            </div>
+                            <a id="contact-button" href="/contact">Contactez moi</a>
+                        </div>
+                        
+                    </div>
+                    <Image className="desktop" src={pp_entier} alt="photo de profil"/>
+                </div>
             </div>
-        </div>
         </>
     )
 }
--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -4,7 +4,7 @@ import '@/styles/header.css'
 import '@/styles/1024.css'
 import '@/styles/1400.css'
 import '@/styles/body.css'
-import '@/styles/contact.css'
+// import '@/styles/contact.css'
 import '@/styles/device.css'
 import '@/styles/footer.css'
 import Header from 'components/header'
@@ -12,8 +12,8 @@ import Header from 'components/header'
 export default function App({ Component, pageProps }) {
   return(
     <>
-    <Header/>
-    <Component {...pageProps} />
+      <Header/>
+      <Component {...pageProps} />
     </>
   )
 }
