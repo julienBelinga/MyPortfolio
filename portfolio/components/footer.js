411be0cdc46ee5c5bad27bcf4b0f29e5457a88df
julien belinga <julienbelinga29@gmail.com>
2023-03-10 17:20:15 +0100

    add footer

---
+++ portfolio/components/footer.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import github from '/assets/img/github.png'
+import linkedIn from '/assets/img/linkedIn.png'
+import malt from '/assets/img/malt.png'
+
+export default function footer() {
+  return (
+    <>
+        <footer>
+            <ul>
+                <li className="link"><Link href="/assets/CV freelance.pdf" download>Mon CV</Link></li>
+                <li className="link"><Link href="/contact">Contact</Link></li>
+                <li className="link"><Link href="/mentions-legales">Mentions légales</Link></li>
+                <ul className="icon-set">
+                    <li><Link href="https://github.com/julienBelinga" target="blank"><Image className="icon" src={github} alt="Github"/></Link></li>
+                    <li><Link href="https://www.linkedin.com/in/julien-belinga/" target="blank"><Image className="icon" src={linkedIn} alt="LinkedIn"/></Link></li>
+                    <li><Link href="https://www.malt.fr/profile/julienbelinga?overview=true" target="blank"><Image className="icon" src={malt} alt="Malt"/></Link></li>
+                </ul>
+            </ul>
+        </footer>
+    </>
+  )
+}
--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -4,13 +4,16 @@ import '@/styles/presentation.scss'
 import '@/styles/skills.scss'
 import '@/styles/project.scss'
 import '@/styles/device.scss'
+import '@/styles/footer.scss'
 import Navbar from '@/components/navbar'
+import Footer from 'components/footer'
 
 export default function App({ Component, pageProps }) {
   return(
     <>
       <Navbar/>
       <Component {...pageProps} />
+      <Footer/>
     </>
   )
 }
