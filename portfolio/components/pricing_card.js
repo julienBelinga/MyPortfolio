6e9ed601cc34e06c656afd1e65c5254b1ad73dce
Ayoub <abassou643@gmail.com>
2023-03-12 22:21:04 +0100

    Fix pricing responsive

--- portfolio/components/pricing_card.js
+++ portfolio/components/pricing_card.js
@@ -5,20 +5,24 @@ import { useRouter } from 'next/router';
 
 export default function pricing_card(props) {
 
-    const router = useRouter();
 
-  return (
-    <>
-        <div id="vitrine">
-            <div>
-                <p className="popular"><strong>POPULAIRE</strong></p>
-                <h3>{props.title}</h3>
+	return (
+		<>
+			<div
+				id="vitrine"
+				className={`${props.promote && "promote"}`}
+			>
+				<div className="header-container">
+					<p className="popular">
+						<strong>POPULAIRE</strong>
+					</p>
+					<h3>{props.title}</h3>
                 <p>à partir de<br/>
                 <strong>{props.price}</strong></p>
                 <hr/>
-            </div>
-            <ul>
-                <li>
+				</div>
+				<ul>
+					<li>
                     <Image src={check} width={20} alt="checkmark png"/>
                     {props.info1}
                 </li>
@@ -32,13 +36,13 @@ export default function pricing_card(props) {
                 </li>
                 <li>
                     <Image src={check} width={20} alt="checkmark png"/>
-                    {props.info4}
-                </li>
-            </ul>
-            <div id="button-container">
+						{props.info4}
+					</li>
+				</ul>
+				<div id="button-container">
                 <button onClick={() => router.push('/contact')}>Demander un devis</button>
-            </div>
-        </div>
-    </>
-  )
+				</div>
+			</div>
+		</>
+	)
 }
