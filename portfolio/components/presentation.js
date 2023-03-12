8904026198f92586fe0d65c28a144b9992e0f33f
julien belinga <julienbelinga29@gmail.com>
2023-03-12 13:02:27 +0100

    update presentatiton

--- portfolio/components/presentation.js
+++ portfolio/components/presentation.js
@@ -8,27 +8,29 @@ function Presentation(){
             <div>
                 <div id="bg-title">Developpeur web</div>
             </div>
-            <div id="presentation-info">
-                <div>
-                    <h1>Julien BELINGA</h1> 
-                    <div style={{display: "flex", flexDirection: "column"}}>
-                        <div className="wrapper-txt">
-                            <p className="static-txt">Je suis un</p>
-                            <ul className="dynamic-txts">
-                                <li><span>Développeur Front end</span></li>
-                                <li><span>Développeur Back end</span></li>
-                                <li><span>DEVOPS</span></li>
-                                <li><span>Freelance.</span></li>
-                            </ul>
+            <div id="info-container">
+                <div id="presentation-info">
+                    <div>
+                        <h1>Julien BELINGA</h1> 
+                        <div style={{display: "flex", flexDirection: "column"}}>
+                            <div className="wrapper-txt">
+                                <p className="static-txt">Je suis un</p>
+                                <ul className="dynamic-txts">
+                                    <li><span>Développeur Front end</span></li>
+                                    <li><span>Développeur Back end</span></li>
+                                    <li><span>DEVOPS</span></li>
+                                    <li><span>Freelance.</span></li>
+                                </ul>
+                            </div>
+                            <a id="contact-button" href="/contact">Contactez moi</a>
                         </div>
-                        <a id="contact-button" href="/contact">Contactez moi</a>
+                        
                     </div>
-                    
                 </div>
-            </div>
-            <div id="profil">
-                <div id="profile-mobile-bg"></div>
-                <Image id="pp" src={pp_entier} alt="photo de profil" priority/>
+                <div id="profil">
+                    <div id="profile-mobile-bg"></div>
+                    <Image id="pp" src={pp_entier} alt="photo de profil" priority/>
+                </div>
             </div>
         </div>
         </>
