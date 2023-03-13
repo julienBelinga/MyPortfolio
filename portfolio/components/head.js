be7048638112ca27236e1bef0911b6b5a9b976f3
julien belinga <julienbelinga29@gmail.com>
2023-03-13 13:44:38 +0100

    try improve SEO

--- portfolio/components/head.js
+++ portfolio/components/head.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Script from 'next/script'
 
 function PageHead({ title, description }) {
   return (
@@ -8,7 +7,7 @@ function PageHead({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <Script src="https://use.fontawesome.com/e073950383.js" strategy="lazyOnload" />
+      <script src="https://use.fontawesome.com/e073950383.js" strategy="lazyOnload" />
       <style dangerouslySetInnerHTML={{ __html: "@import url('https://use.fontawesome.com/e073950383.css')" }} />
     </Head>
   );
