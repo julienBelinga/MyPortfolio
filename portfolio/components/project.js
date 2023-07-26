0ed82e023be1b89dc20bc9c36475072833b2d38e
julien belinga <julienbelinga29@gmail.com>
2023-07-26 12:39:16 +0200

    update

--- portfolio/components/project.js
+++ portfolio/components/project.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import Description from 'components/project_description'
-import Device from 'components/project_device'
+import React from 'react';
+import Description from 'components/project_description';
+import Device from 'components/project_device';
 
 export default function Project(props) {
+  const even = props.even ? 'even' : 'odd';
 
-  const even = props.even ? "even" : "odd";
+  // Définir img3 sur img2 si img3 n'est pas renseigné
+  const img3 = props.img3 || props.img2;
 
   return (
     <>
       <div id="project" className={`${even}`}>
-          <Device
-              img1={props.img1}
-              img2={props.img2}
-              legend={props.legend}
-          />
-          <Description 
-              title={props.title}
-              description={props.description}
-          />
+        <Device
+          img1={props.img1}
+          img2={props.img2}
+          img3={img3}
+          legend={props.legend} 
+        />
+        <Description
+          title={props.title} 
+          description={props.description} 
+        />
       </div>
     </>
-  )
+  );
 }
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -5,10 +5,13 @@ import PageHead from 'components/head'
 import PricingMultiple from 'components/pricing_multiple'
 import einden_phone_screen from "assets/img/einden_phone_screen.png"
 import einden_laptop_screen from "/assets/img/einden_laptop_screen.png"
-import project_phone_screen from "assets/img/project_phone_screen.png"
-import project_desktop_screen from "assets/img/project_desktop_screen.png"
+import project_phone_screen from "assets/img/project_phone_screen.jpg"
+import project_desktop_screen from "assets/img/project_desktop_screen.jpg"
 import touchdown_phone_screen from "assets/img/touchdown_phone_screen.png"
 import touchdown_laptop_screen from "assets/img/touchdown_laptop_screen.png"
+import bar from "assets/img/bar1.jpg"
+import bar2 from "assets/img/bar2.jpg"
+import bar_phone_screen from "assets/img/bar_phone.jpg"
 import { useEffect, useState } from "react"
 import { useTranslation } from 'react-i18next'
 
@@ -24,6 +27,7 @@ export default function Home() {
   const description_einden = <p>{t('project.description1')}</p>
   const description_portfolio = <p>{t('project.description2')}</p>
   const description_getTouchdown = <p>{t('project.description3')}</p>
+  const description_bar = <p>{t('project.description4')}</p>
 
 	return (
 		<>
@@ -31,6 +35,14 @@ export default function Home() {
 			<Presentation/>
 			<Skills/>
 			<section id="project-container">
+				<Project
+					even={true}
+					title={t('project.title4')}
+					description={description_bar}
+					img1={bar_phone_screen}
+					img2={bar}
+					img3={bar2}
+				/>
 				<Project
 					even={false}
 					title={t('project.title1')}
