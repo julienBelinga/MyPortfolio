5d0ed59c591255232b438e99134c3c7bd2043377
julien belinga <julienbelinga29@gmail.com>
2025-06-03 01:48:58 +0200

    Refactor portfolio: remove almost everything

--- portfolio/app/[lang]/page.tsx
+++ portfolio/app/[lang]/page.tsx
@@ -1,24 +1,7 @@
-import { Metadata } from "next";
-import { languages } from "../i18n/settings";
-import HomeContent from "@components/home-content";
-
-export async function generateStaticParams() {
-  return languages.map((lang) => ({ lang }));
-}
-
-export const metadata: Metadata = {
-  title: "Julien Belinga - Portfolio",
-  description: "Portfolio de Julien Belinga, développeur web freelance Nantais",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
-};
-
 export default function Home({
   params: { lang },
 }: {
   params: { lang: string };
 }) {
-  return <HomeContent />;
+  return <>Hello world</>;
 }
